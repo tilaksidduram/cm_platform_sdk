f9917bae43cdc8a2d96ce17724f6e1d89efefa8d
Ali B <abittin@gmail.com>
2016-02-18 17:28:51 +0200

    cmsdk: add Sound tile to QS (2/2)
    Change-Id: I345dc67739482593977deb296d7486234be3e499

--- src/java/org/cyanogenmod/internal/util/QSConstants.java
+++ src/java/org/cyanogenmod/internal/util/QSConstants.java
@@ -70,7 +70,8 @@ public class QSConstants {
     public static final String TILE_APP_KILL = "kill_app";
     public static final String TILE_POWER = "power_menu";
     public static final String TILE_THEMES = "themes";
-    public static final String TILE_APP_PICKER = "app_picker";					
+    public static final String TILE_APP_PICKER = "app_picker";
+    public static final String TILE_SOUND = "sound";
 
     public static final String DYNAMIC_TILE_NEXT_ALARM = "next_alarm";
     public static final String DYNAMIC_TILE_IME_SELECTOR = "ime_selector";
@@ -132,6 +133,7 @@ public class QSConstants {
 	STATIC_TILES_AVAILABLE.add(TILE_APP_KILL);
         STATIC_TILES_AVAILABLE.add(TILE_APP_PICKER);
         STATIC_TILES_AVAILABLE.add(TILE_CAFFEINE);
+        STATIC_TILES_AVAILABLE.add(TILE_SOUND);
 
         TILES_AVAILABLE.addAll(STATIC_TILES_AVAILABLE);
 
